eb63b9b7365a0fcbe1f62ad848f7eb6ff9fd3ef9
nrkramer <nrkramer@sb165-w7-6454.smcm.edu>
2015-11-13 02:45:17 -0500

    Added MainMenu and Implemented in Driver.

---
+++ TicTacToe/src/main/Driver.java
@@ -0,0 +1,26 @@
+
+package main;
+
+public class Driver {
+
+	public static String version = "1.0.0";
+	
+	public static void main(String[] args) {
+		// open interface (which will inherently for this
+		// kind of project, take control of the logic flow
+		// in this program)
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				ticTacToeInterface();
+			}
+			
+		}); // this makes sure that the GUI is run on a separate thread
+		// not that it really matters...
+	}
+	
+	public static void ticTacToeInterface() {
+		MainMenu menu = new MainMenu();
+	}
+}
